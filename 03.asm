bf5dfc6bbad4560230c48781d923bbd9368fdea7
Eric <erichmatt@gmail.com>
2018-04-15 22:34:31 -0400

    got a subroutine working

--- 03.asm
+++ 03.asm
@@ -1,13 +1,23 @@
 	ORG 00H
-	SEX 8
-	LDI 10H
-	PHI 8
-	LDI 00H
-	PLO 8
-	MARK
-	
-	LDI 02H
-	ADI 03H
-	STR 8
-	OUT 4
+	ldi low main
+	plo 3
+	ldi high main
+	phi 3
+	sep 3
+main	SEQ		;Set Q
+	LDI low delay
+	PLO 7
+	ldi high delay
+	phi 7
+	SEP 7
+	REQ		;Reset Q
+	SEP 7
+	BR main
+exitd	sep 3
+delay	LDI 20H
+	PHI 0fh
+loop	DEC 0fh
+	GHI 0fh
+	bz exitd
+	br loop
 	END
